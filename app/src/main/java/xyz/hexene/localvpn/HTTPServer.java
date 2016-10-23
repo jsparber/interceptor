50f2f030228790b5df8787d8afbc158cfdffe544
Julian Sparber <julian@sparber.net>
2016-10-23 20:08:34 +0200

    [refactor] cleand up code

--- app/src/main/java/xyz/hexene/localvpn/HTTPServer.java
+++ app/src/main/java/xyz/hexene/localvpn/HTTPServer.java
@@ -1,55 +1,19 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package xyz.hexene.localvpn;
 
-import android.os.Handler;
-import android.os.Message;
-import android.support.v4.content.res.TypedArrayUtils;
 import android.text.TextUtils;
 import android.util.Log;
-import android.util.StringBuilderPrinter;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.io.PrintStream;
-import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
-import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-/**
- * Implementation of a very basic HTTP server. The contents are loaded from the assets folder. This
- * server handles one request at a time. It only supports GET method.
- * https://developer.android.com/samples/PermissionRequest/src/com.example.android.permissionrequest/SimpleWebServer.html
- */
 public class HTTPServer implements Runnable {
 
     public static final String BROADCAST_HTTP_LOG = "xyz.hexene.localvpn.HTTP_SERVER";
@@ -60,11 +24,6 @@ public class HTTPServer implements Runnable {
     private int elementToremove;
 
     /**
-     * The port number we listen to
-     */
-    private final int mPort;
-
-    /**
      * The {@link java.net.ServerSocket} that we listen to.
      */
     private ServerSocket mServerSocket;
@@ -73,14 +32,13 @@ public class HTTPServer implements Runnable {
      * WebServer constructor.
      */
     public HTTPServer(int port, InetAddress orgDestAddr, int origPort, HashMap<Integer, Integer> proxyPorts, int sourcePort) {
-        mPort = port;
         this.proxyPorts = proxyPorts;
         this.elementToremove = sourcePort;
         this.originalDestinationAddress = orgDestAddr;
         this.originalDestinationPort = origPort;
 
         try {
-            mServerSocket = new ServerSocket(mPort);
+            mServerSocket = new ServerSocket(port);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -144,27 +102,21 @@ public class HTTPServer implements Runnable {
         Socket serverSocket = null;
 
         serverSocket = new Socket(this.originalDestinationAddress, this.originalDestinationPort);
-        //Map<String, String> requestHeader = new HashMap<String, String>();
 
         sendLog("\nNew request:\n");
-
 
         outputClient = clientSocket.getOutputStream();
         inputClient = clientSocket.getInputStream();
         outputServer = serverSocket.getOutputStream();
         inputServer = serverSocket.getInputStream();
 
+        //Create threads for the pipes
         Thread oneWay = pipe(inputClient, outputServer);
         Thread otherWay = pipe(inputServer, outputClient);
 
-
-        sendLog("\nRequest headers: \n");
         oneWay.start();
-
-
-        sendLog("\nResposne headers: \n");
-
         otherWay.start();
+
         //wait for the pipes to finish
         try {
             oneWay.join();
@@ -175,45 +127,9 @@ public class HTTPServer implements Runnable {
             serverSocket.close();
             clientSocket.close();
         }
-
     }
 
-            /*
-            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String line;
-            while (!TextUtils.isEmpty(line = reader.readLine())) {
-                if (line.startsWith("GET /")) {
-                    int start = line.indexOf('/') + 1;
-                    int end = line.indexOf(' ', start);
-                    route = line.substring(start, end);
-                }
-                else {
-                    String[] part = line.split(": ");
-                    if (part.length == 2)
-                        requestHeader.put(part[0], part[1]);
-                }
-
-                Log.d(TAG, line);
-                sendLog("   " + line + "\n");
-            }
-
-            for (Map.Entry<String,String> entry : requestHeader.entrySet()) {
-                urlConnection.setRequestProperty(entry.getKey(), entry.getValue());
-            }
-                for (Map.Entry<String, List<String>> entry : header.entrySet()) {
-
-                    String key = (entry.getKey() == null) ? "" : (entry.getKey() + ": ");
-                    if (entry.getKey() != null && entry.getKey().equals("Transfer-Encoding") && !entry.getValue().get(0).equals("chunked"))
-                        output.write(("Transfer-Encoding: chunked\r\n").getBytes());
-                    else
-                        output.write((key + entry.getValue().get(0) + "\r\n").getBytes());
-                }
-
-    }
-    */
-
-
-    //maby should use pipedInputStream and pipedOutputStream
+    //maybe should use pipedInputStream and pipedOutputStream
     private Thread pipe(final InputStream in, final OutputStream out) {
         final byte[] buffer = new byte[16384];
         Thread runner = new Thread(new Runnable() {
@@ -223,7 +139,6 @@ public class HTTPServer implements Runnable {
                 try {
                     len = in.read(buffer);
                 } catch (IOException e) {
-                    //e.printStackTrace();
                 }
                 try {
                     while (out != null && in != null) {
@@ -233,16 +148,13 @@ public class HTTPServer implements Runnable {
                     }
 
                 } catch (IOException e) {
-                    //e.printStackTrace();
                 } catch (ArrayIndexOutOfBoundsException e) {
                     //output conection is closed
-                    //e.printStackTrace();
                 } finally {
                     try {
                         in.close();
                         out.close();
                     } catch (IOException e) {
-                        //e.printStackTrace();
                     }
 
                 }
@@ -269,9 +181,6 @@ public class HTTPServer implements Runnable {
 
     private void sendLog(String output) {
         Log.d(TAG, output);
-        //Message msg = Message.obtain();
         LoggerOutput.println(output);
-        //msg.obj = output;// Some Arbitrary object
-        //this.mHandler.sendMessage(msg);
     }
 }
